b325ba1f90c017eee186af319470ce45f1b4a15b
VadimKochegin <101050439+VadimKochegin@users.noreply.github.com>
2022-04-08 11:24:05 +0300

    Add files via upload

---
+++ 12.7.h_w.py
@@ -0,0 +1,12 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = []
+money_str = input('введите сумму: ')
+money = float(money_str)
+
+
+for key in per_cent:
+    deposit.append(money * per_cent[key] / 100)
+    print(key, "процент по депозитам=", deposit[-1])
+
+
+print("максимальный депозит=", max(deposit))
